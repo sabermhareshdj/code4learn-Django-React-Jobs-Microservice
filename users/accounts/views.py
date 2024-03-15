b205d95e08eb82c2cb1b385de434b0a2aab6b9b4
djsabermharesh <dj.saber.mharesh@gmail.com>
2024-03-16 02:46:41 +0300

    add resend-activation-code & reset-passwordAPI

--- users/accounts/views.py
+++ users/accounts/views.py
@@ -4,7 +4,16 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth import get_user_model  # get user from settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+
+User = get_user_model()  # CustomsUser 
 
 class UserLoginAPI(ObtainAuthToken):
   
@@ -42,10 +51,59 @@ class ChangePasswordAPI(APIView):
 
 
 class ResendActiovationCodeAPI(APIView):
-  pass
+
+  def post(self,request,*args, **kwargs):
+    email = request.data.get('email')
+    try:
+      user = User.objects.get(email=email)
+    except User.DoesNotExist:
+      return Response({'error':'User with this email does not exist'},status=status.HTTP_404_NOT_FOUND)
+
+    if user.is_active:
+      return Response({'error':'this user is already activate'},status=status.HTTP_400_BAD_REQUEST)
+
+    # create activation link 
+    current_site = get_current_site(request)   #get domain : url 
+    mail_subject = 'Activate Your Account'
+    message = render_to_string('accounts/activation_email.html',{
+      'user' : user ,
+      'domain' : current_site.domain ,
+      'uid' : urlsafe_base64_decode(force_bytes(user.id)) ,
+      'token' : default_token_generator.make_token(user)
+
+    })
+    to_email = user.email
+    send_mail(mail_subject,message,'saber.mharsh@gmail.com',[to_email])
+    return Response({'success':'Activation email has been sent'}, status=status.HTTP_200_OK)
+
+
+
+
+
+
 
 class PasswordResetAPI(APIView):
-  pass 
+    def post(self,request,*args, **kwargs):
+      email = request.data.get('email')
+      try:
+        user = User.objects.get(email=email)
+      except User.DoesNotExist:
+        return Response({'error':'User with this email does not exist'},status=status.HTTP_404_NOT_FOUND)
+
+      # create activation link 
+      current_site = get_current_site(request)   #get domain : url 
+      mail_subject = 'Reset Your Account'
+      message = render_to_string('accounts/password_reset_email.html',{
+        'user' : user ,
+        'domain' : current_site.domain ,
+        'uid' : urlsafe_base64_decode(force_bytes(user.id)) ,
+        'token' : default_token_generator.make_token(user)
+
+      })
+      to_email = user.email
+      send_mail(mail_subject,message,'saber.mharsh@gmail.com',[to_email])
+      return Response({'success':'Activation email has been sent'}, status=status.HTTP_200_OK)
+ 
 
 
 class UserSignupAPI(APIView):
